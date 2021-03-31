d1545fd9c3fb8443d0308f2b274518dcb565226a
SzoDavid <szobonya.david@gmail.com>
2021-03-31 02:32:52 +0200

    dynamic commands, keyv, only check message if its sent in a set channel

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 require('dotenv').config();
 
-const prefix = "wc!";
+const prefix = process.env.PREFIX;
+const ignore_prefix = process.env.IGNORE_PREFIX;
+
+const Keyv = require('keyv');
+const data = new Keyv();    //('mysql://user:pass@localhost:3306/dbname');
+
+const fs = require('fs');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login(process.env.BOT_TOKEN);
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.on('ready', botReady);
-client.on('message', gotMessage)
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+client.login(process.env.BOT_TOKEN);
 
 /* EVENT HANDLERS */
 
-function botReady() {
+client.on('ready', async () => {
+    await data.set('status', 'ready');
+    await data.set('channel', ' ');
     console.log("Bot is ready!");
-}
+});
 
-function gotMessage(msg) {
-    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+client.on('message', async msg => {
+    const channel = await data.get('channel');
 
-    const args = msg.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
+    if (msg.content.startsWith(ignore_prefix) || msg.author.bot || (channel === ' ') === (msg.channel.name === channel)) return;
 
-    if (command === 'ping') {
-    	msg.channel.send('Pong.');
-    } else if (command === 'beep') {
-    	msg.channel.send('Boop.');
+    console.log(msg.content);
+
+    if (msg.content.startsWith(prefix)) {
+        const args = msg.content.slice(prefix.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        if (!client.commands.has(command)) {
+            msg.reply('Unknown command!')
+            return;
+        }
+
+        try {
+            client.commands.get(command).execute(data, msg, args);
+        } catch (error) {
+            console.error(error);
+            msg.reply('There was an error trying to execute that command!');
+        }
+
+        return;
     }
-}
+});
