4c12b4030453ff8d387e555b261dd3a9dcd1fa3c
SzoDavid <szobonya.david@gmail.com>
2021-03-31 23:00:21 +0200

    Admin only, and reset reaction

--- commands/reset_channel.js
+++ commands/reset_channel.js
@@ -1,7 +1,12 @@
 module.exports = {
 	name: 'reset_channel',
 	async execute(data, message, args) {
+		if (!message.member.hasPermission("ADMINISTRATOR")) {
+			message.reply('Only admins can use this command!');
+			return;
+		}
         await data.set('status', 'ready');
 		await data.delete('channel');
+		message.react('✅');
 	},
 };
