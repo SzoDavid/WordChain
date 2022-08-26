c3d115be99c419023cd4c0bce7c92ab6c53a0051
SzoDavid <szobonya.david@gmail.com>
2022-08-26 14:58:39 +0200

    OnStart event

--- game/events.js
+++ game/events.js
@@ -1,13 +1,47 @@
+require('dotenv').config();
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = { OnStart };
 
-function OnStart() {
+async function OnStart(channel, client) {
+    const filter = m => !(m.author.bot || m.content.startsWith(process.env.IGNORE_PREFIX));
+    client.collectors.set(channel.id, channel.createMessageCollector({ filter }));
+
+    const collector = client.collectors.get(channel.id);
+
+	collector.on('collect', message => {
+		OnMessage(message, client)
+	});
+
+	collector.on('end', collected => {
+		OnEnd(collected, client);
+	});
+
+    const query = await client.sequelize.models.Channel.findOne({
+        where: {
+            id: channel.id,
+        },
+    });
+
+    const startEmbed = new EmbedBuilder()
+		.setColor(0xed1c24)
+		.setTitle(`Ready, set, WordChain!`);
+
+    if (query.dataValues.highscore !== 0) {
+        startEmbed.setDescription(`Write a word wich starts with the letter the previous word ends with! Messages starting with \`${process.env.IGNORE_PREFIX}\` will be ignored. Have fun!`);
+    } else {
+        startEmbed.setDescription(`The high score in this channel is ${query.dataValues.highscore}. Break it!`);
+    }
+
+    await channel.send({ embeds: [startEmbed] })
+}
+
+function OnEnd(collected, client) {
+    console.log(collected)
     // TODO
 }
 
-function OnEnd() {
-    // TODO
-}
-
-function OnMessage() {
+function OnMessage(message, client) {
+    console.log(message)
     // TODO
 }
