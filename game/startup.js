2c202d8b1e4b98282d857a4e980d743952574a89
SzoDavid <szobonya.david@gmail.com>
2022-08-29 20:38:32 +0200

    fixed formatting

--- game/startup.js
+++ game/startup.js
@@ -4,18 +4,18 @@ const { Client } = require('discord.js');
 module.exports = { StartUp };
 
 async function StartUp(client) {
-     try {
+    try {
         const query = await client.sequelize.models.Channel.findAll();
-         query.forEach(async record => {
-             const channel = await client.channels.cache.get(record.dataValues.id);
-             if(channel) {
-                 events.OnStart(channel, client);
+        query.forEach(async record => {
+            const channel = await client.channels.cache.get(record.dataValues.id);
+            if(channel) {
+                events.OnStart(channel, client);
             } else {
                 console.log("Channel isn't available (probably because the channel was deleted)."); // TODO: Better managing of this, and probably deleting it from the database.
             }
-         });
-     } catch(e) {
-         console.error("Couldn't start up channels.");
-         console.error(e);
-     }
+        });
+    } catch(e) {
+        console.error("Couldn't start up channels.");
+        console.error(e);
+    }
 }
